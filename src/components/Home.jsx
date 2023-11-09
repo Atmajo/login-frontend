e61b9b1361b949041a7b4c02dd2f272834c1cf89
Atmajo <atmajoc@gmail.com>
2023-11-09 21:26:29 +0530

    update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import {AiOutlineMenu} from "react-icons/ai";
 
 const Home = ({ email }) => {
   return (
     <div>
-      <div>
+      <div className="navbar sm">
         <nav className="flex flex-row justify-between p-5 m-2 bg-slate-500 rounded-xl shadow-xl">
           <h3 className="p-2">{email}</h3>
           <div className="flex flex-row gap-5">
             <ul className="flex flex-row gap-5 p-2">
-              <li><a href="/" className="hover:text-xl hover:border-purple-400 hover:border-b-2 ">HOME</a></li>
-              <li><a href="/" className="hover:text-xl hover:border-purple-400 hover:border-b-2 ">ABOUT</a></li>
-              <li><a href="/" className="hover:text-xl hover:border-purple-400 hover:border-b-2 ">CONTACT</a></li>
+              <li><a href="/" className="text-sm hover:text-xl hover:border-purple-400 hover:border-b-2 ">HOME</a></li>
+              <li><a href="/" className="text-sm hover:text-xl hover:border-purple-400 hover:border-b-2 ">ABOUT</a></li>
+              <li><a href="/" className="text-sm hover:text-xl hover:border-purple-400 hover:border-b-2 ">CONTACT</a></li>
             </ul>
             <div className="gap-2 border-black rounded-md border-2 p-2 hover:bg-slate-200 hover:border-purple-400">
               <ul>
                 <li>
-                  <a href="/login">LOGOUT</a>
+                  <a href="/login" className="text-sm ">LOGOUT</a>
                 </li>
               </ul>
             </div>
