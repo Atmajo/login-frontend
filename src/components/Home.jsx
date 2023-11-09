51441a1032c30a20113e073aa038281da1206002
Atmajo <atmajoc@gmail.com>
2023-11-09 21:29:22 +0530

    update Home.jsx 2.0

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,14 @@ const Home = ({ email }) => {
     <div>
       <div className="navbar sm">
         <nav className="flex flex-row justify-between p-5 m-2 bg-slate-500 rounded-xl shadow-xl">
-          <h3 className="p-2">{email}</h3>
-          <div className="flex flex-row gap-5">
-            <ul className="flex flex-row gap-5 p-2">
+          <h3 className="p-1">{email}</h3>
+          <div className="flex flex-row gap-3">
+            <ul className="flex flex-row gap-3 p-1">
               <li><a href="/" className="text-sm hover:text-xl hover:border-purple-400 hover:border-b-2 ">HOME</a></li>
               <li><a href="/" className="text-sm hover:text-xl hover:border-purple-400 hover:border-b-2 ">ABOUT</a></li>
               <li><a href="/" className="text-sm hover:text-xl hover:border-purple-400 hover:border-b-2 ">CONTACT</a></li>
             </ul>
-            <div className="gap-2 border-black rounded-md border-2 p-2 hover:bg-slate-200 hover:border-purple-400">
+            <div className="gap-1 border-black rounded-md border-2 p-2 hover:bg-slate-200 hover:border-purple-400">
               <ul>
                 <li>
                   <a href="/login" className="text-sm ">LOGOUT</a>
